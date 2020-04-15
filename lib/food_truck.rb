79996c242e493451f0a46d4d68176d01f8d2699b
Michael Gallup <Gallup93@icloud.com>
2020-04-15 10:33:09 -0600

    added check stock method (not fully functioning)

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -4,4 +4,12 @@ class FoodTruck
     @name = name
     @inventory = {}
   end
+
+  def check_stock(item)
+    if @inventory.any?{|stocked_item| item.name == stocked_item[:name]}
+      require "pry";binding.pry
+    else
+      return 0
+    end
+  end
 end
